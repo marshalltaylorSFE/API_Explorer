fcf5be7061b6770cd16d53d24119e3b3b211c4ea
marshalltaylorSFE <marshall.taylor@sparkfun.com>
2017-10-28 16:36:22 -0600

    Fixed async issues more properly

--- nodeApp/src/app/api-data.service.ts
+++ nodeApp/src/app/api-data.service.ts
@@ -8,31 +8,32 @@ import { HttpClient } from '@angular/common/http';
 export class ApiDataService {
   constructor(private http: HttpClient) { }
   
-  getIndex(): Promise<Index> {
-	let tempIndex: Index = new Index;
-	tempIndex.indexElements = [];
-	let resIndex: any;
-	let tempIndexElement: IndexElement;
-    console.log("Getting json for index.");
-    this.http.get('assets/indexTemplate.json').subscribe(data => {
-		resIndex = data;
-		for (let file of resIndex.files) {
-			tempIndexElement = new IndexElement;
-			tempIndexElement.name = file.name;
-			tempIndexElement.path = file.path;
-			tempIndex.indexElements.push(tempIndexElement);
-			console.log(file);
-		}
-		console.log(tempIndex);
+  async getIndex(): Promise<Index> {
+	return new Promise<Index>(resolve => {
+		let tempIndex: Index = new Index;
+		tempIndex.indexElements = [];
+		let resIndex: any;
+		let tempIndexElement: IndexElement;
+//  	  console.log("Getting json for index.");
+		this.http.get('assets/indexTemplate.json').subscribe(data => {
+			resIndex = data;
+			for (let file of resIndex.files) {
+				tempIndexElement = new IndexElement;
+				tempIndexElement.name = file.name;
+				tempIndexElement.path = file.path;
+				tempIndex.indexElements.push(tempIndexElement);
+//				console.log(file);
+			}
+			resolve(tempIndex);
+		});
+		tempIndex.debugString = 'Debuggy';
 	});
-	tempIndex.debugString = 'Debuggy';
-	return Promise.resolve(tempIndex);
   }
 
-  getData( reqIndex: Index ): Promise<Data[]> {
-    return new Promise(resolve => {
-      // Simulate server latency with 2 second delay
-      setTimeout(() => {
+  async getData( reqIndex: Index ): Promise<Data[]> {
+	return new Promise<Data[]>(resolve => {
+			let filesFetched: number = 0;
+			let filesRequested: number = 0;
 			let tempData: Data;
 			let tempDataArray: Data[] = [];
 			let resData: any;
@@ -41,80 +42,28 @@ export class ApiDataService {
 			//debugger;
 			//Iterate through the passed index
 			for (let file of reqIndex.indexElements) {
+				filesRequested++;
 				console.log(file);
 				//debugger;
 				this.http.get('assets/' + file.path).subscribe(data => {
 					resData = data;
 					tempData = new Data;
+					tempData.name = resData.name;
 					tempData.entries = [];
-					//tempData.name = resData.
+					for( let entry of resData.entries ){
+						tempData.entries.push(entry);
+					};
 					//debugger;
-					
+					filesFetched++;
+					tempDataArray.push(tempData);
+					console.log(tempDataArray);
+					if(filesFetched == filesRequested)
+					{
+						console.log(tempDataArray);
+						resolve(tempDataArray);
+					}
 				});
 			}
-
-		  resolve(fakeDataArray);
-		  }, 2000);
-    });
-//	let tempData: Data;
-//	let tempDataArray: Data[] = [];
-//	let resData: any;
-//	
-//	console.log("Waiting...");
-//	setTimeout(()=>{console.log("Done")}, 4000)
-//	console.log("Getting json for data from index");
-//    console.log(JSON.stringify(reqIndex));
-//	//debugger;
-//	//Iterate through the passed index
-//	for (let file of reqIndex.indexElements) {
-//		console.log(file);
-//		//debugger;
-//		this.http.get('assets/' + file.path).subscribe(data => {
-//			resData = data;
-//			tempData = new Data;
-//			tempData.entries = [];
-//			//tempData.name = resData.
-//			//debugger;
-//			
-//		});
-//	}
-//	return Promise.resolve(fakeDataArray);
+	});
   }
-  
-  querryData(): void {
-//    this.http.get('assets/indexTemplate.json').subscribe(data => {
-//		this.tempVar = data;
-//		console.log(this.tempVar.files[0].name);
-//	});
-//	  
-  }
-//  getFullData(): void {
-//	let tempPointer: number = 0;
-//	let tempFilePointer: number = 0;
-//	//Clear index
-//	this.currentIndex.indexElements = [];
-//    // Make the HTTP request:
-//    console.log("Getting json data for index.");
-//    this.http.get('assets/indexTemplate.json').subscribe(data => {
-//    // Read the result field from the JSON response.
-//    this.indexData = data;
-//	console.log(this.indexData);
-//	//Now get each contained filename.
-//	for (let file of this.indexData.files) {
-//		console.log("Getting json data for:");
-//		console.log(file.path);
-//		this.http.get('assets/' + file.path).subscribe(data => {
-//			tempFilePointer = this.bodyData.push(data) - 1;
-//			//Build index for viewer
-//			tempPointer = this.currentIndex.indexElements.push({ name: this.bodyData[tempFilePointer].name, subElements: [], expandable: false }) - 1;
-//			for (let entry of this.bodyData[tempFilePointer].entries) {
-//				this.currentIndex.indexElements[tempPointer].subElements.push({ name: entry.name, subElements: [], expandable: false });
-//				this.currentIndex.indexElements[tempPointer].expandable = true;
-//			}
-//			});
-//		}
-//	this.currentIndex.debugString = 'processed';
-//	console.log(this.bodyData);
-//    });
-//  }
 }
--- nodeApp/src/app/api-explorer/api-explorer.component.ts
+++ nodeApp/src/app/api-explorer/api-explorer.component.ts
@@ -17,17 +17,44 @@ export class ApiExplorerComponent implements OnInit {
   constructor( private apiDataService: ApiDataService ) {
   }
 
-  ngOnInit() {
-	//this._index = this.apiDataService.getIndex();
-	//this.apiDataService.getData(this.apiDataService.getIndex());
-	this.apiDataService.getIndex().then(retIndex => {
-		this._index = retIndex;
-		this.apiDataService.getData(retIndex).then(retData => {
-			this._data = retData;
-			});
+  async ngOnInit() {
+	//By example, this should seem to work:
+//	this.apiDataService.getIndex().then(retIndex => {
+//		//debugger;
+//		this._index = retIndex;
+//		console.log(this._index);
+//	});
+
+	//But it doesn't pause the code, this seems to work better
+	this._index = await this.apiDataService.getIndex();
+	console.log(this._index);
+
+	this.apiDataService.getData(this._index).then(retData =>
+	{
+		this._data = retData;
 		this.loaded = true;
+		console.log(this._index);
+		console.log(this._data);
+		//Build the rest of the index
+		for (let indexThing of this._index.indexElements) {
+			console.log(indexThing);
+			for (let file of this._data) {
+				if (indexThing.path == file.name)
+				{
+					console.log( 'match, expanding' );
+					indexThing.expandable = true;
+					indexThing.subElements = [];
+					for (let fileThing of file.entries){
+						let tempElement: IndexElement = new IndexElement;
+						tempElement.name = fileThing.name;
+						indexThing.subElements.push(tempElement);
+					}
+				}
+			};
+		};
 	});
-	//this._dataBody = this.apiDataService.getDataBody();
 	console.log("ApiExplorerComponent ngOnInit() has run.");
   }
+  
+
 }
